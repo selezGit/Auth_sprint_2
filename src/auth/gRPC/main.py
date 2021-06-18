8dea104ea784f0891945bdf35a2e22bb4b0102ac
Asman Malikov <asmanmalikov@gmail.com>
2021-06-18 15:59:13 +0300

    добавил сервис Role

--- src/auth/gRPC/main.py
+++ src/auth/gRPC/main.py
@@ -6,6 +6,7 @@ from db.db import init_db
 
 from services.user import UserService
 from services.auth import AuthService
+from services.role import RoleService
 
 from loguru import logger
 
@@ -14,11 +15,13 @@ def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=6))
     auth_pb2_grpc.add_AuthServicer_to_server(AuthService(), server)
     auth_pb2_grpc.add_UserServicer_to_server(UserService(), server)
+    auth_pb2_grpc.add_RoleServicer_to_server(RoleService(), server)
     server.add_insecure_port('[::]:50051')
 
     server.start()
     logger.info('Server startup.')
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     init_db()
