3f8647390bbd854881948bdf17844d56a1c1c82c
Asman Malikov <asmanmalikov@gmail.com>
2021-06-18 16:02:13 +0300

    remove_role and append_role

--- src/auth/gRPC/crud/user.py
+++ src/auth/gRPC/crud/user.py
@@ -1,10 +1,11 @@
 from typing import Any, Dict
 
 import bcrypt
-from db.db_models import User
+from db.db_models import User, Role
 from sqlalchemy.orm import Session
 
 from crud.base import CRUDBase
+from loguru import logger
 
 
 class CRUDUser(CRUDBase):
@@ -17,13 +18,14 @@ class CRUDUser(CRUDBase):
     def get_by(self, db: Session, **kwargs):
         return db.query(User).filter_by(**kwargs).first()
 
-    def create(self, db: Session, *, obj_in: Dict) -> User:
+    def create(self, db: Session, *, obj_in: Dict, role=None) -> User:
         password = obj_in.pop('password')
-
         hash_bytes = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
         password_hash = hash_bytes.decode('utf-8')
         db_obj = self.model(**obj_in)
         db_obj.password_hash = password_hash
+        if role:
+            db_obj.roles.append(role)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
@@ -35,6 +37,22 @@ class CRUDUser(CRUDBase):
         db.commit()
         db.refresh(db_obj)
         return db_obj
+
+    def is_admin(self, db: Session, user_id):
+        user = db.query(User).get(user_id)
+        for role in user.roles:
+            if role.name == 'admin':
+                return True
+        return False
+
+    def remove_role(self, db: Session, user: User, role: Role):
+        user.roles.remove(role)
+        db.commit()
+
+    def append_role(self, db: Session, user: User, role: Role):
+        user.roles.append(role)
+        db.add(user)
+        db.commit()
 
     def update(
             self,
